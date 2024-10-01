70492e55b48d4d7e39ef161e80eeb19edd11c002
andregp222 <88045678+andregp222@users.noreply.github.com>
2024-10-01 20:04:15 -0300

    solved(maybe) not functioning

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='OHIOgrapher',
-    version='0.1',
-    description='creates a graph, kinda janky method',
-    author='André Glycerio',
-    packages=find_packages(),
-)
