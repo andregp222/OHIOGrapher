7b35d80f1179f82af96eecbfe9ed0ac45b040473
andregp222 <88045678+andregp222@users.noreply.github.com>
2024-10-07 22:25:51 -0300

    a

--- build/lib/OHIOgrapher/Grapher.py
+++ build/lib/OHIOgrapher/Grapher.py
@@ -1,4 +1,3 @@
-import pygame as pg
 import OHIOgrapher.Drawer as dr
 import sys
 import numpy as np
@@ -21,10 +20,6 @@ TEXT = 4
 def DrawGraph(data:list):
     ren = dr.Renderer(width, height)
 
-    win = pg.display.set_mode((width, height))
-    pg.display.set_caption("Graph")
-    win.fill(GraphColors[BACKGROUND])
-
     ren.Fill(GraphColors[BACKGROUND])
 
     dataprocessed = []
@@ -45,44 +40,29 @@ def DrawGraph(data:list):
     j = 0
 
     #drawBase
-    pg.draw.line(win, GraphColors[GUIDE_LINE], (50,0), (50,height),guideLineThickness)
-    pg.draw.line(win, GraphColors[GUIDE_LINE], (0,height-50), (width,width-50),guideLineThickness)
-
     ren.DrawLine((100,0), (100,height), GraphColors[GUIDE_LINE], guideLineThickness)
     ren.DrawLine((0,height-50), (width,width-50), GraphColors[GUIDE_LINE], guideLineThickness)
-
-    pg.font.init()
-    my_font = pg.font.SysFont('Arial', fontSize)
     i = 0
     while j < len(dataprocessed):
-        text_surface = my_font.render(str(data[j][1]), True, GraphColors[TEXT])
-        win.blit(text_surface, (0,dataprocessed[j][1]))
-        ren.DrawText(str(data[j][1]), (0,dataprocessed[j][1]), GraphColors[TEXT], 3)
+        ren.DrawText(str(data[j][1]), (0,dataprocessed[j][1]), GraphColors[TEXT], 2)
 
-        text_surface = my_font.render(str(data[j][0]), True, GraphColors[TEXT])
-        win.blit(text_surface, (dataprocessed[j][0],height-48))
-        ren.DrawText(str(data[j][1]), (dataprocessed[j][0],height-45), GraphColors[TEXT], 3)
+        ren.DrawText(str(data[j][1]), (dataprocessed[j][0],height-45), GraphColors[TEXT], 2)
         
         if j > 0 :
-            pg.draw.line(win, GraphColors[GRAPH_LINE],dataprocessed[j-1], dataprocessed[j], graphLineThickness)
             ren.DrawLine(dataprocessed[j-1], dataprocessed[j], GraphColors[GRAPH_LINE], graphLineThickness)
 
         j+=1
     
     while i < len(dataprocessed):
-        pg.draw.circle(win, GraphColors[POINTS], dataprocessed[i], PointThickness, 0)
+        #draw a circle
 
         i+=1
 
     # now save the drawing
     # can save as .bmp .tga .png or .jpg
     fname = "graph.png"
-    pg.image.save(win, fname)
     print("file: " + fname + " has been saved!")
-    ren.Save("graph2.png")
-
-    pg.display.flip()
-    pg.quit()
+    ren.Save(fname)
     pass
 
 DrawGraph([(0,0),(100,100),(200,300)])
